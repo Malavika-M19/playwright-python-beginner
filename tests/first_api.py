dc42fd03b8b5bfbcec74a40bb30825046f9f2dd8
Malavika M <malavikaaravind2022@gmail.com>
2026-08-22 00:04:42 -0500

    Chapter 2-Pytest

--- tests/first_api.py
+++ tests/first_api.py
@@ -1,10 +1,12 @@
-from playwright.sync_api import sync_playwright
+# from playwright.sync_api import sync_playwright
 
-with sync_playwright() as p:
-    requests=p.request.new_context()
-    response = requests.get("https://automationintesting.online/api/branding")
-    response_json = response.json()
-    response_code = response.status
-    assert response_code == 200, f"Expected status code 200, but got {response_code}"
-    assert response_json["name"] == "Shady Meadows B&B", f"Expected name 'Shady Meadows B&B', but got '{response_json['name']}'"
-    requests.dispose()
+# with sync_playwright() as p:
+#     requests=p.request.new_context()
+#     response = requests.get("https://automationintesting.online/api/branding")
+#     response_json = response.json()
+#     response_code = response.status
+#     assert response_code == 200, f"Expected status code 200, but got {response_code}"
+#     assert response_json["name"] == "Shady Meadows B&B", f"Expected name 'Shady Meadows B&B', but got '{response_json['name']}'"
+#     requests.dispose()
+
+from playwright.sync_api import expect
---
+++ tests/test_first_ui.py
@@ -0,0 +1,19 @@
+# from playwright.sync_api import sync_playwright
+
+# """ Verify that the title of the page is correct"""
+# with sync_playwright() as p:
+#     browser = p.chromium.launch(headless=False)
+#     context = browser.new_context()
+#     page = context.new_page()
+#     page.goto("https://automationintesting.online/")
+#     print(page.title())
+#     expected_title = "Restful-booker-platform demo"
+#     assert page.title() == expected_title, f"Expected title '{expected_title}', but got '{page.title()}'"
+#     browser.close()
+
+from playwright.sync_api import expect
+def test_page_title(page):
+    page.goto("https://automationintesting.online/")
+    print(page.title())
+    expect(page).to_have_title("Restful-booker-platform demo")
+    page.get_by_role
